24dcdfb98ca93d713cabc4ff1bc9b788e0f9d99b
P.S. Li <p.s.li19980101@gmail.com>
2017-08-30 20:35:17 +0800

    update Calculator.py

--- Python/Calculator.py
+++ Python/Calculator.py
@@ -5,29 +5,35 @@ import numpy as np
 class Calculator(Frame):
 	def __init__(self, master=None):
 		Frame.__init__(self, master)
-		self.num = np.zeros((4, 3), Button)
+		self.num = np.zeros((12), Button)
+		self.count = 0
+		self.tmp = 0
 		self.grid()
 		self.useCalculator()
 
 	def useCalculator(self):
+		def countNum(a, b):
+			self.tmp = a*3+b+1
+			self.outputField.insert(0,self.tmp)
+
 		self.outputField = Entry(self, width=50)
 		self.outputField.grid(row=0, column=1, columnspan=4)
 
 		for i in range(4):
 			for j in range(3):
 				if i < 3 :
-					self.num[i][j] = Button(self, text=i*3+j+1)
-					self.num[i][j].grid(row=3-i, column=j)
+					self.num[i*3+j] = Button(self, text=i*3+j+1, command=countNum(i,j))
+					self.num[i*3+j].grid(row=3-i, column=j)
 				else:
 					if j == 0 :
-						self.num[i][j] = Button(self, text=".")
-						self.num[i][j].grid(row=4, column=j)
+						self.num[i*3+j] = Button(self, text=".")
+						self.num[i*3+j].grid(row=4, column=j)
 					elif j == 1 :
-						self.num[i][j] = Button(self, text="0")
-						self.num[i][j].grid(row=4, column=j)
+						self.num[i*3+j] = Button(self, text="0")
+						self.num[i*3+j].grid(row=4, column=j)
 					else:
-						self.num[i][j] = Button(self, text="=")
-						self.num[i][j].grid(row=4, column=j)
+						self.num[i*3+j] = Button(self, text="=")
+						self.num[i*3+j].grid(row=4, column=j)
 		
 		self.plusButton = Button(self, text="+")
 		self.plusButton.grid(row=1, column=4)
