f6514e1ec0ad249feeac82ba8494386d9c399517
P.S. Li <p.s.li19980101@gmail.com>
2017-11-23 00:54:49 +0800

    finish crawler-Text

--- Python/Text.py
+++ Python/Text.py
@@ -2,16 +2,38 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-# 讀取網頁
-res = requests.get('https://cecilspeaks.tumblr.com/post/56680281610/episode-1-pilot')
-soup = BeautifulSoup(res.text, "html.parser")
-
-# 找到標題
-txt_name = soup.find('h2', 'post-title').get_text()
-
-# 寫入TXT
-file = open( './'+txt_name+'.txt', 'w', encoding='UTF-8')
-for str in soup.find_all('p'):
-    file.write(str.get_text())
-    file.write("\n\n")
+# 讀取Links文件
+file = open('Links.txt', 'r')
+result_link = list()
+for line in open('Links.txt'): 
+    line = file.readline().replace('\n', '')
+    result_link.append(line)
+print (result_link)
 file.close()
+
+file = open('Title.txt', 'r')
+result_title = list()
+for line in open('Title.txt'): 
+    line = file.readline().replace('\n', '')
+    result_title.append(line)
+print (result_title)
+file.close()
+
+for i in range(len(result_link)):
+    # 讀取網頁
+    res = requests.get(result_link[i])
+    soup = BeautifulSoup(res.text, "html.parser")
+
+    # 找到標題
+    #txt_name = soup.find('h2', 'post-title').get_text()
+
+    # 寫入TXT
+    file = open( './WtNV/'+result_title[i]+'.txt', 'w', encoding='UTF-8')
+    file.write('TITLE：'+result_title[i]+'\n\n\n')
+    for str in soup.find_all('p'):
+        if str.get_text() == "Audio available at: nightvale.libsyn.com" :
+            break
+        else:
+            file.write(str.get_text())
+            file.write("\n\n")
+    file.close()
